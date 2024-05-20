269debd79fed48b1b7954df7d680f57bd19ea931
ye1n <uyeinyein@gmail.com>
2024-05-20 15:48:45 +0900

    feat: Header 로고 및 스타일 추가

--- src/routes/layout/Header.tsx
+++ src/routes/layout/Header.tsx
@@ -1,8 +1,27 @@
+import Logo from "../../assets/images/logo/logo.png";
+import TextLogo from "../../assets/images/logo/textLogo.png";
+import { FiChevronDown } from "react-icons/fi";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="bg-[#111625] py-1 px-[10px] flex justify-between">
+      <div className="flex items-center gap-[10px]">
+        <img className="w-6 h-6 rounded" src={Logo} alt="" />
+        <img className="w-[87px] h-[19px]" src={TextLogo} alt="" />
+      </div>
+      <div className="flex items-center gap-3">
+        <input
+          placeholder="통합 검색"
+          className="bg-white bg-opacity-10 rounded outline-none text-white py-0.5 px-1"
+        />
+        <div className="flex items-center text-white gap-1">
+          <p className="font-bold">홍길동</p>
+          <p className="text-sm">님</p>
+          <FiChevronDown size={16} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
