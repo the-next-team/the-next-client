1b815efc8d7e1eaeeed970da56ba9e855d6a4778
bonjin <bonjin.app@gmail.com>
2024-05-21 15:03:26 +0900

    chore: add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
 import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import AppRouter from "./routes";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <RecoilRoot>
-      <HashRouter>
-        <AppRouter />
-      </HashRouter>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <HashRouter>
+          <AppRouter />
+        </HashRouter>
+      </RecoilRoot>
+    </QueryClientProvider>
   );
 }
 
