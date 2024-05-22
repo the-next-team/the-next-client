2dd03d31a5ef0536ce9bc576df3d2868bb25e10b
bonjin <bonjin.app@gmail.com>
2024-05-22 10:23:23 +0900

    feat: tab 색상 변경

--- src/routes/layout/Layout.tsx
+++ src/routes/layout/Layout.tsx
@@ -40,15 +40,15 @@ function Layout() {
       <main className="flex w-full h-[calc(100vh-32px)]">
         <Sidebar tabMenu={tabMenu} setTabMenu={setTabMenu} />
         <div className="w-full h-full">
-          <div className="overflow-x-auto bg-white">
-            <div className="flex border-r divide-x w-fit border-slate-200">
+          <div className="overflow-x-auto bg-black-200">
+            <div className="flex border-r divide-x w-fit">
               {tabMenu.map((tab, index) => (
                 <button key={index} onClick={() => setActiveTab(tab.href)}>
                   <div
-                    className={`flex items-center h-full gap-2 px-4 py-1 ${
+                    className={`flex items-center h-full gap-2 px-4 py-1  ${
                       tab.href === activeTab
-                        ? "bg-slate-100 border-b-0"
-                        : "bg-transparent hover:bg-slate-100border-b border-slate-200"
+                        ? "bg-white"
+                        : "bg-black-200"
                     }`}
                   >
                     <p className="text-xs whitespace-nowrap">{tab.name}</p>
