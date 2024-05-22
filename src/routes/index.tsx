6bb5f6585d39ee8c5100d2b12de265429c7af8f4
ye1n <uyeinyein@gmail.com>
2024-05-22 10:53:56 +0900

    fix: index 라우터 Suspense 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 
@@ -7,13 +7,15 @@ const Error404 = lazy(() => import("../pages/error/Error404Page"));
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/main/*" element={<Layout />} />
-      <Route path="*" element={<Navigate replace={true} to="/404" />} />
-      {/* <Route path="popup" element={<Popup />} /> */}
-      <Route path="/404" element={<Error404 />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/main/*" element={<Layout />} />
+        <Route path="*" element={<Navigate replace={true} to="/404" />} />
+        {/* <Route path="popup" element={<Popup />} /> */}
+        <Route path="/404" element={<Error404 />} />
+      </Routes>
+    </Suspense>
   );
 };
 
