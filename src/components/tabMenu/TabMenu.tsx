30c0082cf12643d12b30d98179536ff412fe010d
bonjin <bonjin.app@gmail.com>
2024-05-22 15:00:33 +0900

    feat: tab 높이 수정

--- src/components/tabMenu/TabMenu.tsx
+++ src/components/tabMenu/TabMenu.tsx
@@ -15,16 +15,16 @@ const TabMenu = ({
     <div className="overflow-x-auto bg-black-200">
       <div className="flex border-r divide-x w-fit">
         {tabMenu.map((tab, index) => (
-          <button key={index} onClick={() => setActiveTab(tab.href)}>
+          <button className="h-8 cursor-pointer" key={index} onClick={() => setActiveTab(tab.href)}>
             <div
-              className={`flex items-center h-full gap-2 px-4 py-1  ${
+              className={`flex items-center h-full gap-2 px-4 ${
                 tab.href === activeTab ? "bg-white" : "bg-black-200"
               }`}
             >
               <p className="text-xs whitespace-nowrap">{tab.name}</p>
               {tab.href !== "home/dashboard" && (
                 <div
-                  className="p-0.5 rounded-full"
+                  className="rounded-full cursor-pointer"
                   onClick={(e) => {
                     e.stopPropagation();
                     handleTabClose(tabMenu, setTabMenu, tab.href);
