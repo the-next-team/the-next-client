e1dc620e678644a324964bd5dd91f34e7fd68ea8
ye1n <uyeinyein@gmail.com>
2024-05-23 13:59:38 +0900

    feat: Header Toolbar 추가 및 검색바 스타일 적용

--- src/routes/layout/Header.tsx
+++ src/routes/layout/Header.tsx
@@ -1,6 +1,8 @@
 import { FiChevronDown } from "react-icons/fi";
 import Logo1 from "../../assets/images/logo/logo-smartsb-01.png";
 import Logo2 from "../../assets/images/logo/logo-smartsb-02.png";
+import { Icon } from "@iconify/react";
+import Toolbar from "../../components/header/Toolbar";
 
 function Header() {
   return (
@@ -10,10 +12,21 @@ function Header() {
         <img className="w-[87px] h-[19px]" src={Logo2} alt="" />
       </div>
       <div className="flex items-center gap-3">
-        <input
-          placeholder="통합 검색"
-          className="bg-white bg-opacity-10 rounded outline-none text-white py-0.5 px-1"
-        />
+        {/* 툴바 */}
+        <Toolbar />
+        {/* 검색바 */}
+        <div className="w-[230px] flex items-center gap-2 rounded bg-custom-gray-100 bg-opacity-20 px-[6px] py-1">
+          <Icon
+            icon="heroicons:magnifying-glass-solid"
+            width="16"
+            color="white"
+          />
+          <input
+            placeholder="통합 검색"
+            className="w-full text-xs text-white bg-transparent outline-none"
+          />
+        </div>
+        {/* 프로필 */}
         <div className="flex items-center gap-1 text-white">
           <p className="font-bold">홍길동</p>
           <p className="text-sm">님</p>
