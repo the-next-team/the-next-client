88a913e7e5cfb1d079d7eb3cc6c8653d7fe96527
ye1n <uyeinyein@gmail.com>
2024-05-23 11:02:48 +0900

    feat: Sidebar 좌측 툴바 추가

--- src/components/sidebar/NavMenu.tsx
+++ src/components/sidebar/NavMenu.tsx
@@ -64,70 +64,72 @@ function NavMenu({ tabMenu, setTabMenu }: Props) {
   };
 
   return (
-    <ul>
-      {menuItems.map((item, i) => (
-        <li key={i} className="">
-          {/* single menu with no childred */}
-          {!item.child && !item.isHeadr && (
-            <div
-              className="cursor-pointer menu-link bg-black-500"
-              onClick={() => {
-                handleTabOpen(tabMenu, setTabMenu, {
-                  name: item.title ?? "",
-                  href: item.link ?? "",
-                  component: item.element ?? null,
-                });
-              }}
-            >
-              <span className="inline-flex items-center flex-grow-0 mr-3 text-lg icon-box text-slate-600">
-                <Icon icon={item.icon ?? ""} />
-              </span>
-              <div className="flex-grow text-slate-700">{item.title}</div>
-              {item.badge && (
-                <span className="py-1 px-2 text-xs capitalize font-semibold rounded-[.358rem] whitespace-nowrap align-baseline inline-flex bg-slate-900 text-slate-50">
-                  {item.badge}
+    <div className="w-full h-full overflow-y-auto shadow-md">
+      <ul>
+        {menuItems.map((item, i) => (
+          <li key={i} className="">
+            {/* single menu with no childred */}
+            {!item.child && !item.isHeadr && (
+              <div
+                className="cursor-pointer menu-link bg-black-500"
+                onClick={() => {
+                  handleTabOpen(tabMenu, setTabMenu, {
+                    name: item.title ?? "",
+                    href: item.link ?? "",
+                    component: item.element ?? null,
+                  });
+                }}
+              >
+                <span className="inline-flex items-center flex-grow-0 mr-3 text-lg icon-box text-slate-600">
+                  <Icon icon={item.icon ?? ""} />
                 </span>
-              )}
-            </div>
-          )}
-          {/* only for menulabel */}
-          {item.isHeadr && !item.child && (
-            <div className="mt-4 mb-4 text-xs font-semibold uppercase text-slate-800 ">
-              {item.title}
-            </div>
-          )}
-          {/* !!sub menu parent */}
-          {item.child && (
-            <div
-              className={`cursor-pointer flex items-center justify-between px-4 py-2 ${
-                activeSubmenu === i ? "bg-[#f47213] bg-opacity-[0.07]" : ""
-              }`}
-              onClick={() => toggleSubmenu(i)}
-            >
-              <div className="flex items-center gap-2">
-                <Icon icon={item.icon ?? ""} width={16} height={16} />
-                <div className="text-sm font-medium">{item.title}</div>
+                <div className="flex-grow text-slate-700">{item.title}</div>
+                {item.badge && (
+                  <span className="py-1 px-2 text-xs capitalize font-semibold rounded-[.358rem] whitespace-nowrap align-baseline inline-flex bg-slate-900 text-slate-50">
+                    {item.badge}
+                  </span>
+                )}
               </div>
-              <Icon
-                icon="heroicons-outline:chevron-right"
-                className={`duration-300 ${
-                  activeSubmenu === i ? "rotate-90" : ""
+            )}
+            {/* only for menulabel */}
+            {item.isHeadr && !item.child && (
+              <div className="mt-4 mb-4 text-xs font-semibold uppercase text-slate-800 ">
+                {item.title}
+              </div>
+            )}
+            {/* !!sub menu parent */}
+            {item.child && (
+              <div
+                className={`cursor-pointer flex items-center justify-between px-4 py-2 ${
+                  activeSubmenu === i ? "bg-[#f47213] bg-opacity-[0.07]" : ""
                 }`}
-              />
-            </div>
-          )}
-          <SubMenu
-            activeSubmenu={activeSubmenu}
-            item={item}
-            index={i}
-            toggleMultiMenu={toggleMultiMenu}
-            activeMultiMenu={activeMultiMenu}
-            tabMenu={tabMenu}
-            setTabMenu={setTabMenu}
-          />
-        </li>
-      ))}
-    </ul>
+                onClick={() => toggleSubmenu(i)}
+              >
+                <div className="flex items-center gap-2">
+                  <Icon icon={item.icon ?? ""} width={16} height={16} />
+                  <div className="text-sm font-medium">{item.title}</div>
+                </div>
+                <Icon
+                  icon="heroicons-outline:chevron-right"
+                  className={`duration-300 ${
+                    activeSubmenu === i ? "rotate-90" : ""
+                  }`}
+                />
+              </div>
+            )}
+            <SubMenu
+              activeSubmenu={activeSubmenu}
+              item={item}
+              index={i}
+              toggleMultiMenu={toggleMultiMenu}
+              activeMultiMenu={activeMultiMenu}
+              tabMenu={tabMenu}
+              setTabMenu={setTabMenu}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
--- src/routes/layout/Sidebar.tsx
+++ src/routes/layout/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Icon } from "@iconify/react";
 import NavMenu from "../../components/sidebar/NavMenu";
 import { TabMenuListType } from "../../states/layout/layoutAtom";
 
@@ -7,9 +8,40 @@ type Props = {
 };
 
 function Sidebar({ tabMenu, setTabMenu }: Props) {
+  const ToolMenus = [
+    { icon: <Icon icon="heroicons:home" width="16" /> },
+    { icon: <Icon icon="heroicons:star" width="16" /> },
+    {
+      icon: <Icon icon="heroicons:magnifying-glass-solid" width="16" />,
+    },
+    {
+      icon: <Icon icon="heroicons:building-library" width="16" />,
+    },
+    {
+      icon: <Icon icon="heroicons:clipboard-document-list" width="16" />,
+    },
+    { icon: <Icon icon="heroicons:book-open" width="16" /> },
+  ];
+
   return (
-    <div className="w-[248px] h-full bg-white overflow-y-auto">
-      {/* 즐겨찾기 기능 추가 예정 */}
+    <div className="flex w-[248px] h-full bg-white">
+      <div className="flex flex-col items-center h-full gap-2 px-2 py-3 w-11">
+        {ToolMenus.map((menu, i) => (
+          <>
+            <div
+              className={`cursor-pointer border rounded-[6px] p-[6px] ${
+                i == 3
+                  ? "bg-gradient-to-r from-[#f47112] via-[#f35916] to-[#e22f55] text-white"
+                  : "border-primary text-custom-black"
+              }`}
+              key={i}
+            >
+              {menu.icon}
+            </div>
+            {i === 2 && <div className="w-full bg-primary h-[1px]" />}
+          </>
+        ))}
+      </div>
       <NavMenu tabMenu={tabMenu} setTabMenu={setTabMenu} />
     </div>
   );
