d1135ad3dafb28cc29c338d7df1397143f9440de
bonjin <bonjin.app@gmail.com>
2024-05-23 09:58:59 +0900

    chore: titlebar 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import AppRouter from "./routes";
-import TitleBar from "./routes/layout/titleBar/TitleBar";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +10,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <div className="flex flex-col h-full">
-          <TitleBar />
+          {/* <TitleBar /> */}
           <div className="grow">
             <HashRouter>
               <AppRouter />
