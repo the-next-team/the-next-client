01930f6e0901eddbf5a350634ee55f106c051e5e
ye1n <uyeinyein@gmail.com>
2024-05-29 17:58:12 +0900

    feat: Footer 로고 및 저작권 표기 추가

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,28 @@
+import logo from "../../assets/images/logo/logo-gray-scale.png";
 
 function Footer() {
   return (
-    <div className="flex-none bg-white">Footer</div>
-  )
+    <footer>
+      <div className="flex items-center justify-between px-6 py-3 bg-white">
+        <img
+          className="w-[87px] h-[19px] grayscale select-none"
+          src={logo}
+          alt=""
+        />
+        <p className="text-xs text-custom-gray-200">
+          Copyright 2024
+          <a
+            href="#"
+            target="_blank"
+            className="font-medium text-custom-gray-300"
+          >
+            &nbsp;THE NEXT&nbsp;
+          </a>
+          All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
