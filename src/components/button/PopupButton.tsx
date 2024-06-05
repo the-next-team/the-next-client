1e11b37e17d3003e1e04f8737dc953fc9d11d01a
bonjin <bonjin.app@gmail.com>
2024-06-05 09:05:07 +0900

    fix: hashRouter 조건 추가

--- src/components/button/PopupButton.tsx
+++ src/components/button/PopupButton.tsx
@@ -8,8 +8,12 @@ type Props = {
 
 function PopupButton({ path, width, height, children }: Props) {
   const openNewWindow = () => {
+    let route = path;
+    if(window.location.href.includes('#')) {
+      route = `/#${path}`
+    }
     window.electron.ipcRenderer.sendMessage("open-new-window", {
-      route: path, // 새 창에서 열 경로
+      route: route, // 새 창에서 열 경로
       width: width, // 창의 넓이
       height: height, // 창의 높이
     });
