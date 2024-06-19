1b3c457a3ee8901cad6030f4c1e782d10eff0b4b
ye1n <uyeinyein@gmail.com>
2024-06-19 18:06:20 +0900

    refactor: CustomInput 미사용 props 삭제

--- src/components/form/input/CustomInput.tsx
+++ src/components/form/input/CustomInput.tsx
@@ -3,7 +3,6 @@ type Props = {
   disabled?: boolean;
   validate?: string;
   error?: string;
-  icon?: React.ReactNode;
   className?: string;
   description?: string;
 } & React.InputHTMLAttributes<HTMLInputElement>;
@@ -13,10 +12,9 @@ function CustomInput({
   disabled,
   validate,
   error,
-  icon,
   className,
   description,
-  ...props
+  ...rest
 }: Props) {
   return (
     <div>
@@ -31,11 +29,20 @@ function CustomInput({
         <div className="absolute left-0 w-full h-full">
           <input
             id={label}
+            type="text"
             disabled={disabled}
-            className={`${className} ${disabled ? "bg-custom-blue-100" : "bg-transparent"} 
-			${validate ? "ring-green-400 focus:ring-[1.5px]" : error ? " ring-red-400 focus:ring-[1.5px]" : "ring-custom-gray-100 focus:ring-custom-gray-300"} 
+            className={`${className} ${
+              disabled ? "bg-custom-blue-100" : "bg-transparent"
+            } 
+			${
+        validate
+          ? "ring-green-400 focus:ring-[1.5px]"
+          : error
+          ? " ring-red-400 focus:ring-[1.5px]"
+          : "ring-custom-gray-100 focus:ring-custom-gray-300"
+      } 
 		  w-full h-full text-custom-black rounded-[2px] outline-none px-3 duration-100 ring-1`}
-            {...props}
+            {...rest}
           />
         </div>
       </div>
