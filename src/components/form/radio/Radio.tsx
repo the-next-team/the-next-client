4eb648b52a045720ff8ff7febd87429882b51679
ye1n <uyeinyein@gmail.com>
2024-06-20 11:48:30 +0900

    feat: Radio 컴포넌트 추가

---
+++ src/components/form/radio/Radio.tsx
@@ -0,0 +1,44 @@
+type Props = {
+  label?: string;
+  disabled?: boolean;
+  className?: string;
+  onChange?: () => void;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+const Radio = ({
+  label,
+  disabled = false,
+  className,
+  onChange,
+  ...rest
+}: Props) => {
+  return (
+    <div className="flex items-center justify-center gap-2">
+      <input
+        id={label}
+        type="radio"
+        disabled={disabled}
+        className={`${className} ${
+          disabled
+            ? "cursor-default ring-gray-400 border-gray-200 checked:border-gray-400"
+            : "cursor-pointer ring-custom-black border-gray-400 checked:border-custom-black"
+        } select-none duration-100 peer appearance-none rounded-full border bg-white p-2 checked:ring-[6px] checked:ring-offset-2 checked:ring-inset`}
+        {...rest}
+      />
+      {label && (
+        <label
+          htmlFor={label}
+          className={`${
+            disabled
+              ? "cursor-default peer-checked:text-gray-400 text-gray-300"
+              : "cursor-pointer peer-checked:text-custom-black text-custom-gray-200"
+          } duration-100`}
+        >
+          <p className="text-sm select-none">{label}</p>
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default Radio;
