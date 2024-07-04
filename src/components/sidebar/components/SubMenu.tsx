580bd372800f01aeda9599939e878a17f335cb7b
ye1n <uyeinyein@gmail.com>
2024-07-04 13:37:17 +0900

    feat: SubMenu 2depth 문자 추가

--- src/components/sidebar/components/SubMenu.tsx
+++ src/components/sidebar/components/SubMenu.tsx
@@ -29,7 +29,7 @@ function SubMenu({ index, item, activeSubmenu }: Props) {
                 : "text-custom-black"
             } text-sm`}
           >
-            {subItem.name}
+            {`∙ ${subItem.name}`}
           </p>
         </div>
       </li>
