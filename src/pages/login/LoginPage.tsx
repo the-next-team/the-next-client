e6cc5457bc79806bac5961c5bb4882ac465f5284
bonjin <bonjin.app@gmail.com>
2024-07-08 09:11:34 +0900

    refactor: use hook form 적용

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { UserService } from "../../api/services/userService";
 import Input from "../../components/form/input/Input";
@@ -6,32 +6,37 @@ import { storageKey } from "../../constants";
 import useLoading from "../../hooks/useLoading";
 import useUser from "../../hooks/useUser";
 
+type FormValues = {
+  username: string;
+  password: string;
+};
+
 function LoginPage() {
   const navigate = useNavigate();
-  const {fetchUser} = useUser();
+  const { fetchUser } = useUser();
   const { showLoading, hideLoading } = useLoading();
-  const [inputUsername, setInputUsername] = useState("nTree");
-  const [inputPassword, setInputPassword] = useState("1");
-  
-  const handleChange = (e:React.SyntheticEvent<HTMLInputElement>) => {
-    var obj = e.currentTarget;
-    if( obj.id == "username" )
-      setInputUsername(obj.value);
-    else if( obj.id == "password" )
-      setInputPassword(obj.value);
-  };
 
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors, isSubmitting },
+  } = useForm<FormValues>({
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
 
+  const onSubmit = async (values: FormValues) => {
     try {
       showLoading();
-      const req_data = {
-        username: inputUsername,
-        password: inputPassword,
+      const data = {
+        username: values.password,
+        password: values.password,
       };
-      console.log(req_data);
-      const response = await UserService.login(req_data);
+      const response = await UserService.login(data);
       hideLoading();
 
       if (response.status === "OK") {
@@ -46,33 +51,49 @@ function LoginPage() {
         navigate("/", {
           replace: true,
         });
-      }
-      else {
+      } else {
         console.log(response.error);
         alert("[" + response.error.code + "] " + response.error.message);
       }
     } catch (error) {
       hideLoading();
       console.log(error);
-//      setError("fail", {
-//        type: "custom",
-//        message: "아이디 또는 비밀번호가 올바르지 않아요.",
-//      });
+      //      setError("fail", {
+      //        type: "custom",
+      //        message: "아이디 또는 비밀번호가 올바르지 않아요.",
+      //      });
     }
   };
 
   return (
     <div className="flex items-center justify-center w-full h-full bg-white">
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-6">
           <p className="text-2xl font-semibold text-center text-custom-black">
             nTree.next Login
           </p>
         </div>
         <div className="flex flex-col gap-2">
-          <Input type="text" id="username" placeholder="username" onChange={handleChange} value={inputUsername} />
-          <Input type="password" id="password" placeholder="password" onChange={handleChange} value={inputPassword} />
+          <Input
+            type="text"
+            placeholder="username"
+            {...register("username", {
+              required: "아이디를 입력해 주세요.",
+            })}
+          />
+          <Input
+            type="password"
+            placeholder="password"
+            {...register("password", {
+              required: "비밀번호를 입력해 주세요.",
+            })}
+          />
         </div>
+        {(errors.password || errors.password) && (
+          <p className="text-red-500">
+            {"아이디 또는 비밀번호를 다시 입력해주세요"}
+          </p>
+        )}
         <button
           type="submit"
           className="w-full py-1 rounded-[2px] hover:animate-gradient-primary mt-4 bg-custom-black"
