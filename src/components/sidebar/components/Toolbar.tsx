89ca144036bb70145eca451adffadf0b674ad894
ye1n <uyeinyein@gmail.com>
2024-07-10 11:17:40 +0900

    fix: Toolbar 테스트 코드 삭제

--- src/components/sidebar/components/Toolbar.tsx
+++ src/components/sidebar/components/Toolbar.tsx
@@ -90,38 +90,11 @@ function Toolbar() {
           >
             <Icon icon="heroicons:book-open" width="16" />
           </div>
-          <div className="absolute z-10 duration-100 -translate-y-1/2 opacity-0 pointer-events-none top-1/2 left-10 peer-hover:opacity-100">
-            <div className="relative w-[100px] h-[25px] p-0 bg-[#ffe5d2] border rounded-sm after:content-none after:absolute border-[4px 8px 4px 0] block z-10 -left-2 top-2 px-2 py-0.5">
-              <p className="text-sm break-keep whitespace-nowrap text-custom-black">
-                {fav.name}
-              </p>
-            </div>
+          <div className="absolute z-10 px-1 duration-100 -translate-y-1/2 rounded-sm opacity-0 pointer-events-none top-1/2 left-10 peer-hover:opacity-100 bg-primary bg-opacity-30 backdrop-blur">
+            <p className="text-sm break-keep whitespace-nowrap text-custom-black">
+              {fav.name}
+            </p>
           </div>
-          {/* .bubble
-{
-position: relative;
-width: 100px;
-height: 25px;
-padding: 0px;
-background: #FFFFFF;
--webkit-border-radius: 2px;
--moz-border-radius: 2px;
-border-radius: 2px;
-}
-
-.bubble:after
-{
-content: '';
-position: absolute;
-border-style: solid;
-border-width: 4px 8px 4px 0;
-border-color: transparent #FFFFFF;
-display: block;
-width: 0;
-z-index: 1;
-left: -8px;
-top: 8px;
-} */}
         </div>
       ))}
     </div>
