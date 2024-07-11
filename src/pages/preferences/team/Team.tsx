67ecf7d2ac3ba100f945ee184de7c4dcdd38f6d2
bonjin <bonjin.app@gmail.com>
2024-07-11 10:03:04 +0900

    feat: 하단 버튼 추가

--- src/pages/preferences/team/Team.tsx
+++ src/pages/preferences/team/Team.tsx
@@ -211,7 +211,15 @@ function Team() {
         </div>
 
         {/* Footer */}
-        <div></div>
+        <div className="flex justify-end mt-2">
+          <div className="space-xy-5">
+            <Button text="초기화" className="btn-primary btn-sm" />
+            <Button text="등록" className="btn-primary btn-sm" disabled/>
+            <Button text="수정" className="btn-primary btn-sm" />
+            <Button text="삭제" className="btn-danger btn-sm" />
+            <Button text="보기순서변경" className="btn-primary btn-sm" />
+          </div>
+        </div>
       </div>
     </div>
   );
