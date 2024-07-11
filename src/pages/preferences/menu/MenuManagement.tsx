6b2be94313a49d557ed5ccfaac7166586dfaf97b
yyyerin <115134061+hyr0208@users.noreply.github.com>
2024-07-11 09:51:36 +0900

    feat: 상단 버튼 및 realGrid 추가

--- src/pages/preferences/menu/MenuManagement.tsx
+++ src/pages/preferences/menu/MenuManagement.tsx
@@ -4,14 +4,32 @@ import {
   IMenu1Depth
 } from "../../../api/services/menuService";
 import ItemTable from "./components/ItemTable";
+import ButtonPage from "../../example/components/button/ButtonPage";
+import Button from "../../../components/button/ExampleButton";
 // import Table from "./components/Table";
 
 function MenuManagement() {
   const [selected, setSelected] = useState<IMenu1Depth | null>(null); // 선택된 코드
 
   return (
-    <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
+    <div>
+      <div className="flex justify-end gap-3">
+      <Button text="조회" className="btn-primary btn-sm"/>
+      <Button text="초기화" className="btn-primary btn-sm"/>
+      </div>
+      
+      <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
       <div className="">
+        시스템메뉴
+        <ItemTable
+          item={selected}
+          onClick={(item) => {
+            console.log("item", item);
+          }}
+        />
+      </div>
+      <div className="">
+        서브메뉴
         <ItemTable
           item={selected}
           onClick={(item) => {
@@ -20,6 +38,8 @@ function MenuManagement() {
         />
       </div>
     </div>
+    </div>
+    
   );
 }
 
