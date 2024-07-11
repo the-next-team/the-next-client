8f0d4b05990cfbcdea60eee18ca1631704065727
bonjin <bonjin.app@gmail.com>
2024-07-11 15:23:45 +0900

    feat: 한줄 설명 영역 추가

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -46,7 +46,7 @@ function Breadcrumbs() {
   return (
     <>
       {title.length ? (
-        <div className="flex px-4 py-1 mb-2 bg-white rounded">
+        <div className="flex justify-between px-4 py-1 mb-2 bg-white rounded">
           <ul className="flex items-center space-x-1 text-sm">
             {title.map((item, i) => (
               <li
@@ -69,30 +69,41 @@ function Breadcrumbs() {
               </li>
             ))}
             {/* 즐겨찾기 */}
-            <Icon
-              icon="heroicons:star-20-solid"
-              width={16}
-              className="cursor-pointer"
-              color={`${
-                favMenu.find((item) => item.href === activeTab)
-                  ? "#FFCE30"
-                  : "#e2e2e2"
-              }`}
-              onClick={() => {
-                if (favMenu.every((f) => f.href !== activeTab)) {
-                  // 즐겨찾기 추가
-                  setFavMenu(() => [
-                    ...favMenu,
-                    { name: title.at(-1) ?? "", href: activeTab },
-                  ]);
-                } else {
-                  // 즐겨찾기 삭제
-                  const newFavs = favMenu.filter((f) => f.href !== activeTab);
-                  setFavMenu(newFavs);
-                }
-              }}
-            />
+            <li>
+              <Icon
+                icon="heroicons:star-20-solid"
+                width={16}
+                className="cursor-pointer"
+                color={`${
+                  favMenu.find((item) => item.href === activeTab)
+                    ? "#FFCE30"
+                    : "#e2e2e2"
+                }`}
+                onClick={() => {
+                  if (favMenu.every((f) => f.href !== activeTab)) {
+                    // 즐겨찾기 추가
+                    setFavMenu(() => [
+                      ...favMenu,
+                      { name: title.at(-1) ?? "", href: activeTab },
+                    ]);
+                  } else {
+                    // 즐겨찾기 삭제
+                    const newFavs = favMenu.filter((f) => f.href !== activeTab);
+                    setFavMenu(newFavs);
+                  }
+                }}
+              />
+            </li>
           </ul>
+          <div className="flex items-center gap-1">
+            <Icon
+              icon="heroicons:exclamation-circle"
+              width={14}
+              className="cursor-pointer"
+              onClick={() => {}}
+            />
+            <p className="text-xs">한줄 설명 입니다.</p>
+          </div>
         </div>
       ) : null}
     </>
