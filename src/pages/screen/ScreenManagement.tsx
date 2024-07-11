1f091608ed14773e1c8382a621d809d3df0bdf1d
yyyerin <115134061+hyr0208@users.noreply.github.com>
2024-07-11 11:02:06 +0900

    feat: 상하단 버튼 추가

--- src/pages/screen/ScreenManagement.tsx
+++ src/pages/screen/ScreenManagement.tsx
@@ -1,14 +1,30 @@
+import Button from "../../components/button/ExampleButton";
 import Table from "./components/Table";
 
 function ScreenManagement() {
   return (
     <div>
+      <div className="flex justify-end">
+        <div className="flex gap-2 mb-2">
+          <Button text="조회" className="btn-primary btn-sm" />
+          <Button text="초기화" className="btn-primary btn-sm" />
+          <Button text="엑셀" className="btn-primary btn-sm" />
+        </div>
+      </div>
+
       {/* <Card> */}
       <div>
-        <Table
-        />
+        <Table />
+        <div className="flex justify-end mt-2">
+          <div className="space-xy-5">
+            <Button text="초기화" className="btn-primary btn-sm" />
+            <Button text="등록" className="btn-primary btn-sm" disabled />
+            <Button text="수정" className="btn-primary btn-sm" />
+            <Button text="삭제" className="btn-danger btn-sm" />
+            <Button text="보기순서변경" className="btn-primary btn-sm" />
+          </div>
+        </div>
       </div>
-      {/* </Card> */}
     </div>
   );
 }
