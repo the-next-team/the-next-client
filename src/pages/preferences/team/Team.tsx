14772c50e6073b3d7aed427eb62b58dca6baadee
bonjin <bonjin.app@gmail.com>
2024-07-11 10:57:37 +0900

    feat: form 초기화, register 연결

--- src/pages/preferences/team/Team.tsx
+++ src/pages/preferences/team/Team.tsx
@@ -3,6 +3,7 @@ import { GridView, LocalDataProvider, ValueType } from "realgrid";
 import { ApiResponseStats } from "../../../api/models/common/apiResponseStats";
 import { ITeam, TeamService } from "../../../api/services/teamService";
 import Button from "../../../components/button/ExampleButton";
+import HeaderForm from "./components/HeaderForm";
 
 function Team() {
   const [teamList, setTeamList] = useState<ITeam[]>([]);
@@ -192,12 +193,12 @@ function Team() {
     <div>
       <div className="verflow-x-auto ">
         {/* Header */}
-        <div className="flex justify-end">
-          <div className="flex gap-2 mb-2">
-            <Button text="조회" className="btn-primary btn-sm" />
-            <Button text="초기화" className="btn-primary btn-sm" />
-            <Button text="엑셀" className="btn-primary btn-sm" />
-          </div>
+        <div>
+          <HeaderForm
+            onSubmit={(data) => {
+              console.log(data);
+            }}
+          />
         </div>
 
         <div className="inline-block min-w-full align-middle">
@@ -214,7 +215,7 @@ function Team() {
         <div className="flex justify-end mt-2">
           <div className="flex gap-2 mb-2">
             <Button text="초기화" className="btn-primary btn-sm" />
-            <Button text="등록" className="btn-primary btn-sm" disabled/>
+            <Button text="등록" className="btn-primary btn-sm" disabled />
             <Button text="수정" className="btn-primary btn-sm" />
             <Button text="삭제" className="btn-danger btn-sm" />
             <Button text="보기순서변경" className="btn-primary btn-sm" />
