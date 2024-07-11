260a1aa5e32a513e025082ccb46f4606d716c0cf
yyyerin <115134061+hyr0208@users.noreply.github.com>
2024-07-11 10:18:37 +0900

    feat: 하단 버튼 추가

--- src/pages/preferences/menu/MenuManagement.tsx
+++ src/pages/preferences/menu/MenuManagement.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 // import Card from "../../components/card/Card";
-import {
-  IMenu1Depth
-} from "../../../api/services/menuService";
+import { IMenu1Depth } from "../../../api/services/menuService";
 import ItemTable from "./components/ItemTable";
 import ButtonPage from "../../example/components/button/ButtonPage";
 import Button from "../../../components/button/ExampleButton";
@@ -14,35 +12,50 @@ function MenuManagement() {
   return (
     <div>
       <div className="flex justify-end">
-      <div className="space-xy-5">
-      <Button text="조회" className="btn-primary btn-sm"/>
-      <Button text="초기화" className="btn-primary btn-sm"/>
+        <div className="flex gap-2 mb-2">
+          <Button text="조회" className="btn-primary btn-sm" />
+          <Button text="초기화" className="btn-primary btn-sm" />
+        </div>
       </div>
-      
-      </div>
-      
+
       <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
-      <div className="">
-        시스템메뉴
-        <ItemTable
-          item={selected}
-          onClick={(item) => {
-            console.log("item", item);
-          }}
-        />
-      </div>
-      <div className="">
-        서브메뉴
-        <ItemTable
-          item={selected}
-          onClick={(item) => {
-            console.log("item", item);
-          }}
-        />
+        <div>
+          시스템메뉴
+          <ItemTable
+            item={selected}
+            onClick={(item) => {
+              console.log("item", item);
+            }}
+          />
+          <div className="flex justify-end mt-2">
+            <div className="space-xy-5">
+              <Button text="초기화" className="btn-primary btn-sm" />
+              <Button text="등록" className="btn-primary btn-sm" disabled />
+              <Button text="수정" className="btn-primary btn-sm" />
+              <Button text="삭제" className="btn-danger btn-sm" />
+            </div>
+          </div>
+        </div>
+
+        <div className="">
+          서브메뉴
+          <ItemTable
+            item={selected}
+            onClick={(item) => {
+              console.log("item", item);
+            }}
+          />
+          <div className="flex justify-end mt-2">
+            <div className="space-xy-5">
+              <Button text="초기화" className="btn-primary btn-sm" />
+              <Button text="등록" className="btn-primary btn-sm" />
+              <Button text="수정" className="btn-primary btn-sm" disabled />
+              <Button text="삭제" className="btn-danger btn-sm" disabled />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-    
   );
 }
 
