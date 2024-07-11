6595b5d6f21c83f2438f4a21d9f315dee3ad56ec
yyyerin <115134061+hyr0208@users.noreply.github.com>
2024-07-11 15:32:03 +0900

    refactor: 삭제 버튼 색 수정

--- src/pages/screen/ScreenManagement.tsx
+++ src/pages/screen/ScreenManagement.tsx
@@ -20,7 +20,7 @@ function ScreenManagement() {
             <Button text="초기화" className="btn-primary btn-sm" />
             <Button text="등록" className="btn-primary btn-sm" disabled />
             <Button text="수정" className="btn-primary btn-sm" />
-            <Button text="삭제" className="btn-danger btn-sm" />
+            <Button text="삭제" className="btn-primary btn-sm" />
             <Button text="보기순서변경" className="btn-primary btn-sm" />
           </div>
         </div>
