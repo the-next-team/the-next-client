87a999d756b992a9e7d1806c559ede073a894652
ye1n <uyeinyein@gmail.com>
2024-07-11 16:29:47 +0900

    fix: TabMenu 전체닫기 위치 수정

--- src/components/tabMenu/TabMenu.tsx
+++ src/components/tabMenu/TabMenu.tsx
@@ -8,47 +8,49 @@ const TabMenu = () => {
     useTabMenu();
 
   return (
-    <div className="flex-none overflow-x-auto bg-custom-gray-100">
-      <div className="flex w-fit">
-        {tabMenu.map((tab, index) => (
-          <button
-            className="h-8 cursor-pointer"
-            key={index}
-            onClick={() => handleTabOpen(tab)}
-          >
-            <div
-              className={`flex items-center h-full gap-2 px-4 ${
-                tab.href === activeTab
-                  ? "bg-custom-blue-100 rounded-tr-[10px] divide-none"
-                  : "border-r border-custom-blue-100"
-              }`}
+    <div className="relative">
+      <div className="flex-none overflow-x-auto bg-custom-gray-100">
+        <div className="flex w-fit">
+          {tabMenu.map((tab, index) => (
+            <button
+              className="h-8 cursor-pointer"
+              key={index}
+              onClick={() => handleTabOpen(tab)}
             >
-              <p className="text-xs whitespace-nowrap text-custom-black">
-                {tab.name}
-              </p>
-              {/* 메뉴 닫기 (단, 대시보드는 고정)*/}
-              {tab.href !== "dashboard/Dashboard" && (
-                <div
-                  className="rounded-full p-0.5"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    handleTabClose(tab.href);
-                  }}
-                >
-                  <Icon
-                    icon="heroicons:x-mark-16-solid"
-                    width="16"
-                    color="#111625"
-                  />
-                </div>
-              )}
-            </div>
-          </button>
-        ))}
+              <div
+                className={`flex items-center h-full gap-2 px-4 ${
+                  tab.href === activeTab
+                    ? "bg-custom-blue-100 rounded-tr-[10px] divide-none"
+                    : "border-r border-custom-blue-100"
+                }`}
+              >
+                <p className="text-xs whitespace-nowrap text-custom-black">
+                  {tab.name}
+                </p>
+                {/* 메뉴 닫기 (단, 대시보드는 고정)*/}
+                {tab.href !== "dashboard/Dashboard" && (
+                  <div
+                    className="rounded-full p-0.5"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleTabClose(tab.href);
+                    }}
+                  >
+                    <Icon
+                      icon="heroicons:x-mark-16-solid"
+                      width="16"
+                      color="#111625"
+                    />
+                  </div>
+                )}
+              </div>
+            </button>
+          ))}
+        </div>
       </div>
       {/* 전체닫기 */}
       <div
-        className="fixed px-1 py-0.5 flex items-center -translate-y-1/2 bg-white cursor-pointer right-2 top-[66px]"
+        className="absolute px-1 py-0.5 flex items-center -translate-y-1/2 bg-white cursor-pointer right-2 top-4"
         onClick={() => {
           setTabMenu([{ name: "대시보드", href: "dashboard/Dashboard" }]);
           navigate("/dashboard/Dashboard");
