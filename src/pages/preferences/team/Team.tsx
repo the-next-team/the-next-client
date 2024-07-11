1e309dbeb08ba34608469dd4a90a52c3007aec3c
bonjin <bonjin.app@gmail.com>
2024-07-11 10:08:14 +0900

    refactor: 여백 수정

--- src/pages/preferences/team/Team.tsx
+++ src/pages/preferences/team/Team.tsx
@@ -193,7 +193,7 @@ function Team() {
       <div className="verflow-x-auto ">
         {/* Header */}
         <div className="flex justify-end">
-          <div className="space-xy-5">
+          <div className="flex gap-2 mb-2">
             <Button text="조회" className="btn-primary btn-sm" />
             <Button text="초기화" className="btn-primary btn-sm" />
             <Button text="엑셀" className="btn-primary btn-sm" />
@@ -212,7 +212,7 @@ function Team() {
 
         {/* Footer */}
         <div className="flex justify-end mt-2">
-          <div className="space-xy-5">
+          <div className="flex gap-2 mb-2">
             <Button text="초기화" className="btn-primary btn-sm" />
             <Button text="등록" className="btn-primary btn-sm" disabled/>
             <Button text="수정" className="btn-primary btn-sm" />
