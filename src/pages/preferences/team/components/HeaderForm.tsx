7a0ccba18d4cea82b90104f21dabd65b52e52247
bonjin <bonjin.app@gmail.com>
2024-07-12 14:39:44 +0900

    refactor: grid 수정

--- src/pages/preferences/team/components/HeaderForm.tsx
+++ src/pages/preferences/team/components/HeaderForm.tsx
@@ -27,8 +27,8 @@ function HeaderForm({ onSubmit }: Props) {
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="p-2 bg-white rounded">
       <div className="flex">
-        <div>
-          <div className="grid grid-cols-2 gap-3 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 ">
+        <div className="flex-grow">
+        <div className="grid grid-cols-2 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 ">
             <CodeSelect
               label="• 부서(지점)"
               horizontal
@@ -41,7 +41,7 @@ function HeaderForm({ onSubmit }: Props) {
             />
           </div>
         </div>
-        <div className="flex-grow">
+        <div className="">
           <div className="flex justify-end gap-2">
             <Button
               onClick={() => {
