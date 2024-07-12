2c895bd7faea9677f04a1a8118710db1e0048e3a
ye1n <uyeinyein@gmail.com>
2024-07-12 15:29:41 +0900

    refactor: PrivateRoute 루트 경로일경우 대시보드 이동 조건 추가

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,22 +1,33 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { userSelector } from "../states/user/userSelector";
+import useTabMenu from "../hooks/useTabMenu";
 
 function PrivateRoute() {
   const isAuthenticated = useRecoilValue(userSelector);
   const location = useLocation();
+  const { activeTab } = useTabMenu();
 
-  return isAuthenticated ? (
-    <Outlet />
-  ) : (
-    <Navigate
-      to={`${process.env.PUBLIC_URL}/login`}
-      replace={true}
-      state={{
-        from: location,
-      }}
-    />
-  );
+  if (!isAuthenticated) {
+    return (
+      <Navigate
+        to={`${process.env.PUBLIC_URL}/login`}
+        replace={true}
+        state={{ from: location }}
+      />
+    );
+  }
+
+  if (!activeTab) {
+    return (
+      <Navigate
+        to={`${process.env.PUBLIC_URL}/dashboard/Dashboard`}
+        replace={true}
+      />
+    );
+  }
+
+  return <Outlet />;
 }
 
 export default PrivateRoute;
