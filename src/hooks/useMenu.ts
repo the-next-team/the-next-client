88c15d52bad56ad72221730e1663f4deb372642c
ye1n <uyeinyein@gmail.com>
2024-07-12 16:29:02 +0900

    feat: RejectionReasonPage 페이지 추가

--- src/hooks/useMenu.ts
+++ src/hooks/useMenu.ts
@@ -31,6 +31,10 @@ function useMenu() {
             } else if (m.programId === "CMC001U") {
               // 코드관리 -> 공통코드
               m.url = "system/codeManagement/commonCode/CommonCodePage";
+            } else if (m.programId === "CMC020L") {
+              // 코드관리 -> 심사거절사유코드
+              m.url =
+                "system/codeManagement/rejectionReason/RejectionReasonPage";
             } else if (m.programId === "CMC008U") {
               // 기준관리(중앙회) -> 코드매핑(vs중앙회직업코드)
               m.url = "preferences/code/job/JobCodePage";
---
+++ src/pages/system/codeManagement/rejectionReason/RejectionReasonPage.tsx
@@ -0,0 +1,9 @@
+function RejectionReasonPage() {
+  return (
+    <div>
+      <p>심사거절사유코드 페이지 입니다.</p>
+    </div>
+  );
+}
+
+export default RejectionReasonPage;
