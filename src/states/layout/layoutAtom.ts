a72c1fe460775cced45ad933b8fce3d18d71fe69
ye1n <uyeinyein@gmail.com>
2024-07-15 17:33:46 +0900

    refactor: 미사용 코드 및 파일 삭제

--- src/states/layout/layoutAtom.ts
+++ src/states/layout/layoutAtom.ts
@@ -97,31 +97,3 @@ export const customizerTypeState = atom<CustomizerType>({
   default: false,
   effects_UNSTABLE: [persistAtom],
 });
-
-// 현재 활성화된 탭
-export type activeTabType = string;
-export const activeTabTypeState = atom<activeTabType>({
-  key: "activeTabTypeState",
-  default: "home/dashboard",
-  effects_UNSTABLE: [persistAtom],
-});
-
-// 현재 탭 리스트
-export type TabMenuListType = Array<TabMenuType>;
-export type TabMenuType = {
-  name: string;
-  href: string;
-  component?: React.ComponentType | null;
-};
-export const tabMenuTypeState = atom<TabMenuListType>({
-  key: "tabMenuTypeState",
-  default: [{ name: "대시보드", href: "dashboard/Dashboard" }],
-  effects_UNSTABLE: [persistAtom],
-});
-
-export type FavMenuType = Array<{ name: string; href: string }>;
-export const favMenuTypeState = atom<FavMenuType>({
-  key: "favMenuTypeState",
-  default: [],
-  effects_UNSTABLE: [persistAtom],
-});
