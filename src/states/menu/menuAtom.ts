089b7bbd8ef06405d9d2d3342df38121550f7b12
bonjin <bonjin.app@gmail.com>
2024-07-15 17:39:04 +0900

    refactor: 메뉴 데이터 저장 삭제

--- src/states/menu/menuAtom.ts
+++ src/states/menu/menuAtom.ts
@@ -6,14 +6,10 @@ const { persistAtom } = recoilPersist();
 
 export const menuState = atom<IMenu[]>({
   key: "menuState",
-  default: localStorage.getItem("menu")
-    ? JSON.parse(localStorage.getItem("menu")!)
-    : [],
-  effects_UNSTABLE: [persistAtom],
+  default: [],
 });
 
 export const menuSelectedState = atom<IMenu | null>({
   key: "menuSelectedState",
   default: null,
-  effects_UNSTABLE: [persistAtom],
 });
