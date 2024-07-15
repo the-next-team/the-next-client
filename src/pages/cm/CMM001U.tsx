b5921f4e265c438a5902bb8d488eaf57881da555
ye1n <uyeinyein@gmail.com>
2024-07-15 16:17:26 +0900

    fix: CMM001U/CMM002U return문 추가

--- src/pages/cm/CMM001U.tsx
+++ src/pages/cm/CMM001U.tsx
@@ -1,7 +1,9 @@
 function CMM001U() {
-  <div>
-    <p>메뉴관리 페이지 입니다.</p>
-  </div>;
+  return (
+    <div>
+      <p>메뉴관리 페이지 입니다.</p>
+    </div>
+  );
 }
 
 export default CMM001U;
--- src/pages/cm/CMM002U.tsx
+++ src/pages/cm/CMM002U.tsx
@@ -1,7 +1,9 @@
 function CMM002U() {
-  <div>
-    <p>화면관리 페이지 입니다.</p>
-  </div>;
+  return (
+    <div>
+      <p>화면관리 페이지 입니다.</p>
+    </div>
+  );
 }
 
 export default CMM002U;
