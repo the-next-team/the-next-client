e31be23c57fdacaee8002cc0d33f1ee7414a6a7a
ye1n <uyeinyein@gmail.com>
2024-07-16 13:37:01 +0900

    feat: 로그조회 페이지 추가 및 폴더 구조 수정

--- src/pages/cm/CML001L.tsx
+++
@@ -1,9 +0,0 @@
-function CML001L() {
-  return (
-    <div>
-      <p>사용자로그인이력 페이지 입니다.</p>
-    </div>
-  );
-}
-
-export default CML001L;
---
+++ src/pages/cm/CML001L/index.tsx
@@ -0,0 +1,14 @@
+/**
+ * 로그인 이력 페이지
+ * 시스템 > 로그조회 > 로그인 이력
+ * CML001L
+ */
+function CML001L() {
+  return (
+    <div>
+      <p>로그인 이력 페이지 입니다.</p>
+    </div>
+  );
+}
+
+export default CML001L;
