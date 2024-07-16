93d4e415af8da50620e729cdc4e9212010c05c46
yyyerin <115134061+hyr0208@users.noreply.github.com>
2024-07-16 09:21:02 +0900

    feat: RadioGroup추가

--- src/pages/cm/CMC001U/components/CodeTableFooter.tsx
+++ src/pages/cm/CMC001U/components/CodeTableFooter.tsx
@@ -4,6 +4,7 @@ import Button from "../../../../components/button/ExampleButton";
 import Radio from "../../../../components/form/Radio";
 import RadioGroup from "../../../../components/form/RadioGroup";
 import TextInput from "../../../../components/form/TextInput";
+import CodeSelect from "../../../../components/form/CodeSelect";
 
 type FormValues = {
   a: string;
@@ -82,23 +83,202 @@ function CodeTableFooter({ initialValues, onSubmit }: Props) {
             />
           </RadioGroup>
         </div>
-        <TextInput
-          label="권한설정"
-          type="text"
-          horizontal
-          placeholder=""
-          name="e"
-          id="e"
-          register={register}
-        />
+        <div className="grid grid-cols-2 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 ">
+          <TextInput
+            label="대분류코드명"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="코드명약어"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+        </div>
+
+        <div className="grid grid-cols-2 2xl:grid-cols-3 xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 ">
+          <TextInput
+            label="사용여부1용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="사용여부2용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="사용여부3용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="정수1용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="정수2용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="실수1용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="문자1용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="문자2용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="문자3용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="문자4용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <TextInput
+            label="문자5용도"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <RadioGroup horizontal label={"코드셋여부"}>
+            <Radio
+              label="Yes"
+              name="x"
+              value="A"
+              checked={value === "A"}
+              onChange={handleChange}
+            />
+            <Radio
+              label="No"
+              name="x"
+              value="B"
+              checked={value === "B"}
+              onChange={handleChange}
+            />
+          </RadioGroup>
+          <TextInput
+            label="설명"
+            type="text"
+            horizontal
+            placeholder=""
+            name="e"
+            id="e"
+            register={register}
+          />
+          <RadioGroup horizontal label={"주요코드여부"}>
+            <Radio
+              label="Yes"
+              name="x"
+              value="A"
+              checked={value === "A"}
+              onChange={handleChange}
+            />
+            <Radio
+              label="No"
+              name="x"
+              value="B"
+              checked={value === "B"}
+              onChange={handleChange}
+            />
+          </RadioGroup>
+          <RadioGroup horizontal label={"시스템코드여부"}>
+            <Radio
+              label="Yes"
+              name="x"
+              value="A"
+              checked={value === "A"}
+              onChange={handleChange}
+            />
+            <Radio
+              label="No"
+              name="x"
+              value="B"
+              checked={value === "B"}
+              onChange={handleChange}
+            />
+          </RadioGroup>
+          <CodeSelect
+            label="관련대분코드"
+            horizontal
+            codeType="TST"
+            placeholder={"* 선택"}
+            name="c"
+            id="c"
+            register={register}
+          />
+        </div>
       </div>
       <div className="flex justify-end mt-2">
         <div className="space-xy-5">
-          <Button text="새로고침" className="btn-primary btn-sm" />
-          <Button text="초기화" className="btn-primary btn-sm" />
-          <Button text="등록" className="btn-primary btn-sm" disabled />
-          <Button text="수정" className="btn-primary btn-sm" />
-          <Button text="삭제" className="btn-primary btn-sm" />
+          <Button text="새로고침" className="btn-primary btn-sm w-12" />
+          <Button text="초기화" className="btn-primary btn-sm w-12" />
+          <Button text="등록" className="btn-primary btn-sm w-12" disabled />
+          <Button text="수정" className="btn-primary btn-sm w-12" />
+          <Button text="삭제" className="btn-primary btn-sm w-12" />
         </div>
       </div>
     </form>
