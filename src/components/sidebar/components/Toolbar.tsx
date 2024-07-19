bd710b97f86af11153dc426d65667e187c73c929
ye1n <uyeinyein@gmail.com>
2024-07-19 15:53:16 +0900

    feat: Toolbar 메뉴 열려있는 상태시 닫기버튼 추가, 클릭시 메뉴 닫힘

--- src/components/sidebar/components/Toolbar.tsx
+++ src/components/sidebar/components/Toolbar.tsx
@@ -48,55 +48,67 @@ function Toolbar() {
   ];
 
   return (
-    <div className="flex flex-col items-center h-full gap-2 px-2 py-3 w-11">
-      {toolMenus.map((menu, i) => (
-        <div key={i}>
-          <div
-            className={`cursor-pointer rounded-[6px] p-[6px] ${
-              currentSideMenu == menu.name
-                ? "bg-gradient-to-br from-[#f47112] via-[#f35916] to-[#e22f55] text-white shadow-md"
-                : "border border-custom-blue-100 text-custom-black"
-            }`}
-            onClick={() => {
-              //   if (currentSideMenu == menu.name) {
-              //     setCurrentSideMenu("");
-              //   } else {
-              setCurrentSideMenu(menu.name);
-              //   }
-              //   menu.onClick && menu.onClick();
-            }}
-          >
-            <Icon icon={menu.icon} width="16" />
+    <div className="flex flex-col items-center h-full gap-2 px-2 py-3 w-11 justify-between">
+      <div className="flex flex-col items-center h-full gap-2">
+        {toolMenus.map((menu, i) => (
+          <div key={i}>
+            <div
+              className={`cursor-pointer rounded-[6px] p-[6px] ${
+                currentSideMenu == menu.name
+                  ? "bg-gradient-to-br from-[#f47112] via-[#f35916] to-[#e22f55] text-white shadow-md"
+                  : "border border-custom-blue-100 text-custom-black"
+              }`}
+              onClick={() => {
+                //   if (currentSideMenu == menu.name) {
+                //     setCurrentSideMenu("");
+                //   } else {
+                setCurrentSideMenu(menu.name);
+                //   }
+                //   menu.onClick && menu.onClick();
+              }}
+            >
+              <Icon icon={menu.icon} width="16" />
+            </div>
           </div>
+        ))}
+        {favMenu.length > 0 && (
+          <div className="w-full bg-custom-blue-100 h-[1px]" />
+        )}
+        {favMenu.map((fav, i) => (
+          <div key={i} className="relative">
+            <div
+              className={`peer cursor-pointer rounded-[6px] p-[6px] ${
+                activeTab == fav.href
+                  ? "bg-gradient-to-br from-[#f47112] via-[#f35916] to-[#e22f55] text-white shadow-md"
+                  : "border border-custom-blue-100 text-custom-black"
+              }`}
+              onClick={() => {
+                handleTabOpen({
+                  name: fav.name ?? "",
+                  href: fav.href ?? "",
+                });
+              }}
+            >
+              <Icon icon="heroicons:book-open" width="16" />
+            </div>
+            <div className="absolute z-10 px-1 duration-100 -translate-y-1/2 rounded-sm opacity-0 pointer-events-none top-1/2 left-10 peer-hover:opacity-100 bg-[#ffd2c0]">
+              <p className="text-sm break-keep whitespace-nowrap text-custom-black">
+                {fav.name}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      {currentSideMenu && (
+        <div
+          className="cursor-pointer"
+          onClick={() => {
+            setCurrentSideMenu("");
+          }}
+        >
+          <Icon icon="heroicons:x-mark-16-solid" width="20" />
         </div>
-      ))}
-      {favMenu.length > 0 && (
-        <div className="w-full bg-custom-blue-100 h-[1px]" />
       )}
-      {favMenu.map((fav, i) => (
-        <div key={i} className="relative">
-          <div
-            className={`peer cursor-pointer rounded-[6px] p-[6px] ${
-              activeTab == fav.href
-                ? "bg-gradient-to-br from-[#f47112] via-[#f35916] to-[#e22f55] text-white shadow-md"
-                : "border border-custom-blue-100 text-custom-black"
-            }`}
-            onClick={() => {
-              handleTabOpen({
-                name: fav.name ?? "",
-                href: fav.href ?? "",
-              });
-            }}
-          >
-            <Icon icon="heroicons:book-open" width="16" />
-          </div>
-          <div className="absolute z-10 px-1 duration-100 -translate-y-1/2 rounded-sm opacity-0 pointer-events-none top-1/2 left-10 peer-hover:opacity-100 bg-[#ffd2c0]">
-            <p className="text-sm break-keep whitespace-nowrap text-custom-black">
-              {fav.name}
-            </p>
-          </div>
-        </div>
-      ))}
     </div>
   );
 }
