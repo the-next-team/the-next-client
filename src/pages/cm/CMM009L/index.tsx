f5f5d5d0868eed4a34e8057bf328e5bedfc7c56b
yyyerin <115134061+hyr0208@users.noreply.github.com>
2024-07-26 09:40:35 +0900

    feat: RealGridTable 추가

--- src/pages/cm/CMM009L/index.tsx
+++ src/pages/cm/CMM009L/index.tsx
@@ -6,12 +6,142 @@
 import HeaderForm from "./components/HeaderForm";
 import FooterForm from "./components/FooterForm";
 import Table from "./components/Table";
+import { ValueType } from "realgrid";
+import { useRef } from "react";
+import RealGridTable, {
+  RealGridHandle,
+} from "../../../components/table/RealGridTable";
+
+const fields = [
+  {
+    fieldName: "progId",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "progName",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "procDate",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "serialNo",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "targetChk",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "count",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "days",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "param",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "status",
+    dataType: ValueType.TEXT,
+  },
+];
+
+const columns = [
+  {
+    name: "progId",
+    fieldName: "progId",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "프로그램ID",
+  },
+  {
+    name: "progName",
+    fieldName: "progName",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "프로그램명",
+  },
+  {
+    name: "procDate",
+    fieldName: "procDate",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "처리일",
+  },
+  {
+    name: "serialNo",
+    fieldName: "serialNo",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "일련번호",
+  },
+  {
+    name: "targetChk",
+    fieldName: "targetChk",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "대상검수",
+  },
+  {
+    name: "count",
+    fieldName: "count",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "처리건수",
+  },
+  {
+    name: "days",
+    fieldName: "days",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "처리영업일",
+  },
+  {
+    name: "param",
+    fieldName: "param",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "파라메터",
+  },
+  {
+    name: "status",
+    fieldName: "status",
+    type: "data",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "처리상태",
+  },
+];
 
 function CMM009L() {
+  const realGridRef = useRef<RealGridHandle>(null);
+
   return (
-    <div className="overflow-x-auto">
+    <div className="flex flex-col gap-2 relative h-full">
       {/* Header */}
-      <div className="flex-1 mb-2">
+      <div>
         <HeaderForm
           onSubmit={(data) => {
             console.log(data);
@@ -19,11 +149,16 @@ function CMM009L() {
         />
       </div>
 
-      <div className="inline-block min-w-full align-middle">
-        <div className="overflow-hidden">
-          {/* RealGrid */}
-          <Table />
-        </div>
+      {/* Table */}
+      <div className="flex-grow">
+        <RealGridTable
+          ref={realGridRef}
+          fields={fields}
+          columns={columns}
+          onCellDblClicked={(grid, data) => {
+            console.log("onCellDblClicked", data.itemIndex);
+          }}
+        />
       </div>
 
       {/* Footer */}
--- src/pages/cm/CMU003U/index.tsx
+++ src/pages/cm/CMU003U/index.tsx
@@ -3,37 +3,190 @@
  * 시스템 > 메뉴관리 > 객체권한그룹
  * CMU003U
  */
-import React from "react";
+import React, { useRef } from "react";
 import Table from "./components/Table";
 import HeaderForm from "./components/HeaderForm";
 import FooterForm from "./components/FooterForm";
+import { ValueType } from "realgrid";
+import RealGridTable, {
+  RealGridHandle,
+} from "../../../components/table/RealGridTable";
+
+const fields = [
+  {
+    fieldName: "a",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "b",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "c",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "d",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "e",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "f",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "g",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "h",
+    dataType: ValueType.TEXT,
+  },
+  {
+    fieldName: "i",
+    dataType: ValueType.TEXT,
+  },
+];
+
+const columns = [
+  {
+    name: "a",
+    fieldName: "a",
+    type: "data",
+    width: "150",
+    styles: {
+      textAlignment: "center",
+    },
+    header: {
+      text: "사용자명세",
+      showTooltip: false,
+    },
+    renderer: {
+      type: "text",
+      showTooltip: true,
+    },
+  },
+  {
+    name: "b",
+    fieldName: "b",
+    type: "data",
+    width: "150",
+    styles: {
+      textAlignment: "center",
+    },
+    header: {
+      text: "객체권한코드",
+      showTooltip: false,
+    },
+  },
+  {
+    name: "c",
+    fieldName: "c",
+    type: "data",
+    width: "150",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "객체권한명",
+  },
+  {
+    name: "d",
+    fieldName: "d",
+    type: "data",
+    width: "80",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "사용여부",
+  },
+  {
+    name: "e",
+    fieldName: "e",
+    type: "data",
+    width: "80",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "보기순서",
+  },
+  {
+    name: "f",
+    fieldName: "f",
+    type: "data",
+    width: "150",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "등록자",
+  },
+  {
+    name: "g",
+    fieldName: "g",
+    type: "data",
+    width: "150",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "등록일시",
+  },
+  {
+    name: "h",
+    fieldName: "h",
+    type: "data",
+    width: "150",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "변경자",
+  },
+  {
+    name: "i",
+    fieldName: "i",
+    type: "data",
+    width: "150",
+    styles: {
+      textAlignment: "center",
+    },
+    header: "변경일시",
+  },
+];
 
 function CMU003U() {
+  const realGridRef = useRef<RealGridHandle>(null);
+
   return (
-    <div>
-      <div className="flex flex-col gap-2 overflow-x-auto">
-        {/* Header */}
-        <div>
-          <HeaderForm
-            onSubmit={(data) => {
-              console.log(data);
-            }}
-          />
-        </div>
+    <div className="flex flex-col gap-2 relative h-full">
+      {/* Header */}
+      <div>
+        <HeaderForm
+          onSubmit={(data) => {
+            console.log(data);
+          }}
+        />
+      </div>
 
-        {/* Table */}
-        <div>
-          <Table />
-        </div>
+      {/* Table */}
+      <div className="flex-grow">
+        <RealGridTable
+          ref={realGridRef}
+          fields={fields}
+          columns={columns}
+          onCellDblClicked={(grid, data) => {
+            console.log("onCellDblClicked", data.itemIndex);
+          }}
+        />
+      </div>
 
-        {/* Footer */}
-        <div>
-          <FooterForm
-            onSubmit={(data) => {
-              console.log(data);
-            }}
-          />
-        </div>
+      {/* Footer */}
+      <div>
+        <FooterForm
+          onSubmit={(data) => {
+            console.log(data);
+          }}
+        />
       </div>
     </div>
   );
