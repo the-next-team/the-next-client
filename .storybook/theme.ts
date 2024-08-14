13cb152d047e8056388523d14997b892bbdf447f
bonjin <bonjin.app@gmail.com>
2024-08-14 14:40:34 +0900

    docs: 로고 수정

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -1,9 +1,8 @@
 import { create } from "@storybook/theming/create";
-import logo from "../src/assets/images/logo/smart-SFIS-logo@2x.png";
 
 export default create({
   base: "light",
   brandUrl: "/",
-  brandImage: logo,
+  brandImage: require("../src/assets/images/logo/smart-next-black@x2.png"),
   brandTarget: "_self",
 });
