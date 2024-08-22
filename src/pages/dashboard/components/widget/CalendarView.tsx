93223cc62935e55491b5da14448466156fb578fb
ye1n <uyeinyein@gmail.com>
2024-08-22 15:53:25 +0900

    remove: react-calendar 삭제 및 react-datepicker로 변경

--- src/pages/dashboard/components/widget/CalendarView.tsx
+++ src/pages/dashboard/components/widget/CalendarView.tsx
@@ -1,20 +1,67 @@
 import dayjs from "dayjs";
 import { useState } from "react";
-import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+import ko from "date-fns/locale/ko";
+import DatePicker from "react-datepicker";
+import { Icon } from "@iconify/react";
 
 const CalendarView = () => {
-  const [value, onChange] = useState<any>(new Date());
+  const [date, setDate] = useState<Date | null>(new Date());
+
   return (
-    <div>
-      <Calendar
-        onChange={onChange}
-        value={value}
-        calendarType="gregory"
-        formatDay={(locale, date) => dayjs(date).format("DD")}
-        className="rounded text-sm"
-      />
-    </div>
+    <DatePicker
+      locale={ko}
+      inline
+      showIcon
+      icon={<Icon icon="heroicons:calendar-days-solid" width={16} />}
+      selected={date}
+      onChange={(date) => setDate(date)}
+      shouldCloseOnSelect
+      dateFormat="yyyy.MM.dd"
+      renderCustomHeader={({
+        date,
+        decreaseMonth,
+        increaseMonth,
+        prevMonthButtonDisabled,
+        nextMonthButtonDisabled,
+      }) => (
+        <div className="flex items-center justify-between px-3 py-2 border-b rounded select-none">
+          <button
+            type="button"
+            onClick={decreaseMonth}
+            className={`${prevMonthButtonDisabled ? "hidden" : "text-slate-400"}`}
+            disabled={prevMonthButtonDisabled}
+          >
+            <Icon
+              className={`${prevMonthButtonDisabled ? "hidden" : "text-slate-400"}`}
+              icon="heroicons:chevron-left-solid"
+              width={16}
+            />
+          </button>
+          <div className="flex items-center gap-1">
+            <p className="text-sm text-slate-700">
+              <span className="font-medium text-slate-800">
+                {dayjs(date).year()}
+              </span>
+              년
+            </p>
+            <p className="text-sm text-slate-700">
+              <span className="font-medium text-slate-800">
+                {dayjs(date).month() + 1}
+              </span>
+              월
+            </p>
+          </div>
+          <button
+            type="button"
+            onClick={increaseMonth}
+            className={`${nextMonthButtonDisabled ? "hidden" : "text-slate-400"}`}
+            disabled={nextMonthButtonDisabled}
+          >
+            <Icon icon="heroicons:chevron-right-solid" width={16} />
+          </button>
+        </div>
+      )}
+    />
   );
 };
 
