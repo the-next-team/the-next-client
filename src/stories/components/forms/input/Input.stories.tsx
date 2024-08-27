fb1056636482e096f0ead36cccd21078d41756ad
ye1n <uyeinyein@gmail.com>
2024-08-27 16:20:35 +0900

    refactor: Storybook Input Mask Story

--- src/stories/components/forms/input/Input.stories.tsx
+++ src/stories/components/forms/input/Input.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import TextInput from "../../../../components/form/TextInput";
 import { useForm } from "react-hook-form";
+import StringUtil from "../../../../utils/stringUtil";
 
 const meta = {
   title: "Components/Forms/Input",
@@ -63,9 +64,6 @@ const meta = {
     },
     validate: {
       description: "검증",
-    },
-    isMask: {
-      description: "입력 패턴 지정 여부",
     },
     description: {
       description: "설명",
@@ -130,13 +128,13 @@ export const States: Story = () => (
       label="Valid State"
       type="text"
       placeholder="Valid"
-      validate="This is valid state."
+      // validate="This is valid state."
     />
     <TextInput
       label="Invalid State"
       type="text"
       placeholder="Invalid"
-      error="This is invalid state"
+      // error="This is invalid state"
     />
   </div>
 );
@@ -147,104 +145,42 @@ States.args = {
 // Mask
 export const Mask: Story = {
   render: function render() {
-    const { control } = useForm();
+    const {
+      control,
+      register,
+      formState: { errors },
+    } = useForm({
+      mode: "onChange",
+    });
+
     return (
-      <div className="grid grid-cols-2 gap-6">
+      <div className="flex flex-col gap-6">
         <TextInput
-          label="Credit Card(신용카드)"
-          isMask
-          name="CreditCard"
-          control={control}
-          options={{ creditCard: true }}
-          placeholder="0000 0000 0000 0000"
+          label="주민번호"
+          placeholder="######-#######"
+          {...register("residentNumber", {
+            onChange(e) {
+              e.target.value = StringUtil.ResidentNumberFormat(e.target.value);
+            },
+          })}
         />
         <TextInput
-          label="Phone Number(휴대폰번호)"
-          placeholder="Phone Number"
-          options={{ phone: true, phoneRegionCode: "KR" }}
-          isMask
-          name="PhoneNumber"
-          control={control}
+          label="휴대폰번호"
+          placeholder="###-####-####"
+          {...register("phoneNumber", {
+            onChange(e) {
+              e.target.value = StringUtil.PhoneNumberFormat(e.target.value);
+            },
+          })}
         />
         <TextInput
-          label="Date(날짜 년/월/일)"
-          options={{ date: true, datePattern: ["Y", "m", "d"] }}
-          placeholder="YYYY/MM/DD"
-          isMask
-          name="Date1"
-          control={control}
-        />
-        <TextInput
-          label="Date(날짜 년/월)"
-          options={{ date: true, datePattern: ["m", "d"] }}
-          placeholder="YYYY/MM"
-          isMask
-          name="Date2"
-          control={control}
-        />
-        <TextInput
-          label="Time(시간 시:분:초)"
-          options={{ time: true, timePattern: ["h", "m", "s"] }}
-          placeholder="HH:MM:SS"
-          isMask
-          name="Time1"
-          control={control}
-        />
-        <TextInput
-          label="Time(시간 시:분)"
-          options={{ time: true, timePattern: ["h", "m"] }}
-          placeholder="HH:MM"
-          isMask
-          name="Time2"
-          control={control}
-        />
-        <TextInput
-          label="Numeral Formatting(숫자)"
-          options={{ numeral: true }}
-          className="text-right"
-          placeholder="10,000"
-          isMask
-          name="Numeral"
-          control={control}
-        />
-        <TextInput
-          label="Blocks"
-          options={{ blocks: [4, 3, 3], uppercase: true }}
-          placeholder="Block[1,4,7]"
-          isMask
-          name="Blocks"
-          control={control}
-        />
-        <TextInput
-          label="Delimiters"
-          options={{ delimiter: "·", blocks: [3, 3, 3], uppercase: true }}
-          placeholder="Delimiter: '.'"
-          isMask
-          name="CrediDelimiterstCard"
-          control={control}
-        />
-        <TextInput
-          label="Custom Delimiters"
-          options={{
-            delimiters: [".", ".", "-"],
-            blocks: [3, 3, 3, 2],
-            uppercase: true,
-          }}
-          placeholder="Delimiter: ['.', '.', '-']"
-          isMask
-          name="Custom"
-          control={control}
-        />
-        <TextInput
-          label="prefix"
-          options={{
-            prefix: "+82",
-            blocks: [3, 3, 3, 4],
-            uppercase: true,
-          }}
-          isMask
-          name="prefix"
-          control={control}
+          label="신용카드"
+          placeholder="#### #### #### ####"
+          {...register("creditCard", {
+            onChange(e) {
+              e.target.value = StringUtil.CreditCardFormat(e.target.value);
+            },
+          })}
         />
       </div>
     );
